5256007f6aeee13614b9bbb3b7283648c37a2f8e
Jim Hostetler <james.hostetler@gmail.com>
2013-07-19 13:28:06 -0400

    updating the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ducalcrown::Application.config.secret_token = '5710a7d0a4d86151050195b6424457f4d96a10a85824e0a78cc410386e219f1d221a768676cb55531d67ad50908362ab4c3e76572a5f94e3f6dfebed0aecb437'
